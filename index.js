4075e091b632fc925bb15598641857c3fc3c958d
Debmalya1 <debmalya1508@gmail.com>
2023-04-07 23:48:58 +0530

    Set up views for check and user_controller and its route

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const port=8003;
 app.use('/',require('./routes'));
 
 
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server :${err}`);
     }
     console.log(`Server is running on port: ${port}`);
-})
+});
+
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,11 @@ const homeController=require('../controllers/home_controller');
 console.log('Router loaded');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
 
 
 module.exports=router;
+
+// for anty further routes, access from here
+// router.use('/routerName', require('./routerfile));
+
