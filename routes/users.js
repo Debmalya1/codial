2ceb0073666fe766ac93ec717187684b5de5c746
Debmalya1 <debmalya1508@gmail.com>
2023-04-10 19:12:06 +0530

    Branch created and manual authentication success

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express=require('express');
 const router=express.Router();
 
 const usersController=require('../controllers/users_controller');
+//This line can be of diff folder/file i have written by mistake/
+//const { use } = require('./users'); 
 
 router.get('/profile',usersController.profile);
 
@@ -9,7 +11,7 @@ router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);
 
 
-router.post('/create',usersController.create)
-
+router.post('/create',usersController.create);
+router.post('/create-session',usersController.createSession);
 
 module.exports=router;
